7ae08c2add8716177e3231aba0d1e73e23625c51
Daniel-Amadeus <dajg1995@gmail.com>
2021-07-08 17:36:23 +0200

    notes

--- source/code/ImageToNotesConverter.ts
+++ source/code/ImageToNotesConverter.ts
@@ -2,8 +2,8 @@ import Jimp from 'jimp';
 
 export class ImageToNotesInterface {
     protected _image = new Image(100, 100);
-    protected _canvas: HTMLCanvasElement;
     protected _svg = '';
+    protected _originalImage: Jimp;
 
     // A5 - all measurements in mm
     protected _width = 148;
@@ -16,18 +16,13 @@ export class ImageToNotesInterface {
 
     protected _linesPerRow = 5;
     protected _rowDistance = 2;
+    protected _clefWidth = 4;
+
+    protected _clefPath = 'M 0.21647976,7.9844326e-6 C 0.17843267,-9.7982287e-4 0.13393568,0.08987124 0.13541497,0.17089399 0.1357585,0.1955717 0.1435476,0.24562115 0.15491171,0.30847912 0.08324124,0.38265938 1.7136241e-5,0.45878869 1.7136241e-5,0.55867726 -0.00112561,0.65021607 0.05481984,0.76683922 0.18755356,0.7657175 c 0.0203731,-1.4022e-4 0.0387061,-0.002665 0.054992,-0.007011 0.0121355,0.0665316 0.0202049,0.11904182 0.0201558,0.14329887 4.2765e-4,0.0956261 -0.12521124,0.10509053 -0.13123344,0.0501266 0.0264795,-9.1139e-4 0.0475466,-0.0219435 0.0475466,-0.0479533 0,-0.0265706 -0.0219996,-0.0482337 -0.0492993,-0.0482337 -0.0150099,0 -0.0283863,0.00659 -0.03745821,0.0168958 -2.1031e-4,2.1033e-4 -4.5569e-4,4.2065e-4 -6.59e-4,6.3097e-4 -0.0020471,0.002173 -0.0037718,0.004767 -0.0054753,0.007712 -0.0055946,0.009465 -0.009226,0.0230652 -0.0094224,0.0424849 0,0.0800624 0.20243419,0.13159106 0.20243419,-0.0262901 C 0.27944998,0.87613604 0.2703155,0.82222384 0.2570086,0.75429 0.40145021,0.7020603 0.36516279,0.48760265 0.2322537,0.48659311 0.2219479,0.48669827 0.2120138,0.48789711 0.2024582,0.49032311 0.19488653,0.45388819 0.18758167,0.4185262 0.18120185,0.38603157 0.23167193,0.33645884 0.27574125,0.27228985 0.27475277,0.15402622 0.274907,0.06909851 0.24661179,9.9649664e-4 0.21647976,7.9844326e-6 Z M 0.22546029,0.08216647 c 0.0172183,-0.0016616 0.0308891,0.01432288 0.0308891,0.04950961 0.001395,0.0604323 -0.0409425,0.11320892 -0.0913565,0.16628698 -0.004824,-0.0290734 -0.007986,-0.0526294 -0.008329,-0.0666017 0.00143,-0.0944062 0.0400943,-0.14642564 0.0687964,-0.14919489 z m -0.0541155,0.31328749 c 0.006149,0.0316534 0.0127875,0.0650033 0.0194967,0.0985916 -0.0879704,0.0312958 -0.13018883,0.15391307 -0.005475,0.2087859 -0.0760171,-0.0647159 -0.0386009,-0.14132186 0.0162087,-0.15467025 0.0137831,0.0688171 0.0272436,0.13672282 0.0381242,0.19540247 -0.0147715,0.005188 -0.0320109,0.008202 -0.0521456,0.008343 -0.0503439,0 -0.15094754,-0.032039 -0.15094754,-0.15335925 0,-0.1016132 0.07043658,-0.14290627 0.13473854,-0.20309347 z m 0.0427232,0.15051288 c 0.002194,-1.3319e-4 0.004571,-7.711e-5 0.006786,0 0.0915809,0 0.12612263,0.14543713 0.0328662,0.19191815 C 0.24211786,0.6795559 0.2280473,0.61267373 0.21406799,0.54596684 Z'
 
     constructor() {
-        this._canvas = document.getElementById('imagePreview') as HTMLCanvasElement;
-        const context = this._canvas.getContext('2d');
-
-        this._image.onload = () => {
-            this._canvas.width = this._canvas.clientWidth;
-            this._canvas.height = this._canvas.clientHeight;
-            context.fillRect(0, 0, this._canvas.width, this._canvas.height);
-            context.imageSmoothingEnabled = true;
-            context.drawImage(this._image, 0, 0)
-        }
+        this._image = document.getElementById('imagePreview') as HTMLImageElement;
+        this._image.style.width = '100%';
 
         this.addLoadImageButton();
         this.addDownloadButton();
@@ -35,16 +30,71 @@ export class ImageToNotesInterface {
 
     loadImage(path: string): void {
         Jimp.read(path).then((img) => {
-            img.cover(this._canvas.clientWidth, this._canvas.clientHeight,
+            this._originalImage = img;
+            const previewImage = new Jimp(img);
+            previewImage.cover(this._width, this._height,
                 undefined, Jimp.RESIZE_BICUBIC);
-            img.getBase64(Jimp.MIME_PNG, (err, src) => {
+            previewImage.getBase64(Jimp.MIME_PNG, (err, src) => {
                 this._image.src = src;
             });
             this.generateSvg();
         })
     }
 
+    protected weightedGray(img: Jimp): void {
+        for (let x = 0; x < img.getWidth(); x++) {
+            for (let y = 0; y < img.getHeight(); y++) {
+                const color = Jimp.intToRGBA(img.getPixelColor(x, y));
+                const value =
+                    color.r * 0.212 + color.g * 0.7152 + color.b * 0.0722;
+                img.setPixelColor(
+                    Jimp.rgbaToInt(value, value, value, 255, undefined), x, y);
+            }
+        }
+    }
+
+    dither(img: Jimp): void {
+        this.weightedGray(img);
+        const values: number[] = [];
+        for (let y = 0; y < img.getHeight(); y++) {
+            for (let x = 0; x < img.getWidth(); x++) {
+                const color = Jimp.intToRGBA(img.getPixelColor(x, y));
+                const value = color.r / 255.0;
+                values[x + y * img.getWidth()] = value;
+            }
+        }
+
+        for (let y = 0; y < img.getHeight(); y++) {
+            for (let x = 0; x < img.getWidth(); x++) {
+                const value = values[x + y * img.getWidth()];
+                const newValue = value > 0.5 ? 1.0 : 0.0;
+                const error = value - newValue;
+
+                values[(x + 1) + (y + 0) * img.getWidth()]
+                    += error * (7 / 16);
+                values[(x - 1) + (y + 1) * img.getWidth()]
+                    += error * (3 / 16);
+                values[(x + 0) + (y + 1) * img.getWidth()]
+                    += error * (5 / 16);
+                values[(x + 1) + (y + 1) * img.getWidth()]
+                    += error * (1 / 16);
+
+                values[x + y * img.getWidth()] = newValue;
+            }
+        }
+
+        for (let y = 0; y < img.getHeight(); y++) {
+            for (let x = 0; x < img.getWidth(); x++) {
+                const value = values[x + y * img.getWidth()] * 255;
+
+                img.setPixelColor(
+                    Jimp.rgbaToInt(value, value, value, 255, undefined), x, y);
+            }
+        }
+    }
+
     generateSvg(): void {
+
         const svgStart = `<?xml version="1.0" encoding="UTF-8"?>
 <svg width="${this._width}mm" height="${this._height}mm" viewBox="0 0 ${this._width} ${this._height}">
     <rect width="100%" height="100%" fill="white" />
@@ -60,17 +110,39 @@ export class ImageToNotesInterface {
 
         console.log({ rowHeight, rowGap, rowStep });
 
-        for (let i = 0; i < rowCount; i++) {
+        const img = new Jimp(this._originalImage);
+
+        img.cover(this._width / this._lineDistance, this._height / this._lineDistance,
+            undefined, Jimp.RESIZE_BICUBIC);
+
+        this.dither(img);
+
+        for (let r = 0; r < rowCount; r++) {
             const x1 = this._padding;
             const x2 = this._width - this._padding;
-            const rowY = i * rowStep + this._padding;
+            const rowY = r * rowStep + this._padding;
 
             lines += `<line x1="${x1}" y1="${rowY}" x2="${x1}" y2="${rowY + rowHeight}" stroke="black" stroke-width="${this._lineThickness}" />`;
             lines += `<line x1="${x2}" y1="${rowY}" x2="${x2}" y2="${rowY + rowHeight}" stroke="black" stroke-width="${this._lineThickness}" />`;
 
+            lines += `<path fill="black" stroke="none" transform="translate(${this._padding + 0.5 * this._lineDistance} ${rowY - 2 * this._lineDistance}) scale(${rowHeight * 2})" d="${this._clefPath}"`;
+
             for (let l = 0; l < this._linesPerRow; l++) {
                 const y = rowY + l * this._lineDistance;
                 lines += `<line x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="black" stroke-width="${this._lineThickness}" />`;
+
+                for (let i = 0; i < img.getWidth(); i++) {
+                    const x = this._padding + i * this._lineDistance + this._clefWidth * this._lineDistance;
+                    if (x >= x2) {
+                        break;
+                    }
+                    const px = x / this._lineDistance;
+                    const py = y / this._lineDistance;
+                    const pixel = Jimp.intToRGBA(img.getPixelColor(px, py));
+                    if (pixel.r < 128) {
+                        lines += `<circle cx="${x}" cy="${y}" r="${this._lineDistance / 2}" fill="black" />`;
+                    }
+                }
             }
         }
 
@@ -83,7 +155,6 @@ export class ImageToNotesInterface {
         console.log(svgElement);
         svgElement.style.width = '100%';
         svgElement.style.height = '100%';
-        console.log(this._svg);
     }
 
     addLoadImageButton(): void {
@@ -103,7 +174,6 @@ export class ImageToNotesInterface {
                 .join('; ');
 
             const file = loadImageInput.files[0];
-            // console.log(file);
             if (!file) {
                 return;
             }
